b064de50738664f15ab2cebfe35e4b234b13f938
曾卓明 <cengzhuoming@cengzhuomingdeMac-mini.local>
2023-08-12 17:50:38 +0800

    data class

--- base.py
+++ base.py
@@ -7,13 +7,36 @@ cash=pd.read_csv('goodinfo/year/cash.csv')
 dividendRatio=pd.read_csv('goodinfo/year/dividendRatio.csv')
 eps=pd.read_csv('goodinfo/year/eps.csv')
 
+eps4Season=pd.read_csv('goodinfo/last4Season/eps.csv')
+seasonRoe=pd.read_csv('goodinfo/last4Season/roe.csv')
+balance=pd.read_csv('goodinfo/last4Season/balance.csv')
+stock=pd.read_csv('goodinfo/last4Season/stock.csv')
+
 month=pd.read_csv('goodinfo/month/month.csv')
 monthBefore=pd.read_csv('goodinfo/month/monthBefore.csv')
 
-eps4Season=pd.read_csv('goodinfo/last4Season/eps.csv')
-roe4Season=pd.read_csv('goodinfo/last4Season/roe.csv')
-balance=pd.read_csv('goodinfo/last4Season/balance.csv')
-stockYoYcsv=pd.read_csv('goodinfo/last4Season/stockYoY.csv')
-
 todayPrice=baseDfTrans(pbr)[commonDict['price']]
 industry=baseDfTrans(baseInfo)[commonDict['industry']]
+
+class Eps:
+    def __init__(self):
+        self.eps=eps
+
+class DividendRatio:
+    def __init__(self):
+        self.dividendRatio=dividendRatio
+        self.dividendRatioDf=self.getDividendRatioDf()
+    def getDividendRatioDf(self):
+        dividendRatioDf=baseDfTrans(self.dividendRatio).iloc[:,-1]
+        dividendRatioDf.name=lynchDict['dividendRatio']
+        return dividendRatioDf
+
+class SeasonRoe:
+    def __init__(self):
+        self.seasonRoe=seasonRoe
+        self.roeEps=baseDfTrans(self.seasonRoe)[roeEpsList]
+
+class Revenue:
+    def __init__(self):
+        self.month=month
+        self.monthBefore=monthBefore
--- growth.py
+++ growth.py
@@ -1,32 +1,31 @@
 from static import *
 
 class RevenueGrowth:
-    from base import month,monthBefore
+    from base import Revenue
+    revenue=Revenue()
     def __init__(self):
+        self.month=self.revenue.month
+        self.monthBefore=self.revenue.monthBefore
         self.revenueGrowthCount=revenueGrowthNum(self.month,self.monthBefore,lynchDict['revenueGrowthOver30'])
         self.minGrowth=revenueGrowthMin(self.month,self.monthBefore,lynchDict['minGrowth'])
 
 class InnerGrowth:
-    from base import dividendRatio,roe4Season
+    from base import DividendRatio,SeasonRoe
+    dividendRatio=DividendRatio()
+    seasonRoe=SeasonRoe()
     def __init__(self):
-        self.roeEps=baseDfTrans(self.roe4Season)[roeEpsList]
-        self.dividendRatioDf=self.getDividendRatioDf()
-    def getDividendRatioDf(self):
-        dividendRatioDf=baseDfTrans(self.dividendRatio).iloc[:,-1]
-        dividendRatioDf.name=lynchDict['dividendRatio']
-        return dividendRatioDf
+        self.roeEps=self.seasonRoe.roeEps
+        self.dividendRatioDf=self.dividendRatio.dividendRatioDf
     def getInnerGrowth(self):
         innerGrowthDf=concatDf([self.roeEps,self.dividendRatioDf])
         innerGrowth=innerGrowthDf[roeEpsList[0]]*(1-innerGrowthDf[lynchDict['dividendRatio']]/100)
         return innerGrowth
     
 class PastGrowth:
-    from base import eps
+    from base import Eps
+    epsObject=Eps()
     def __init__(self):
+        self.eps=self.epsObject.eps
         self.epsTrend=cagrTrend(self.eps).applymap(to_percentage_with_one_decimal)
         self.epsMinGrowth=self.epsTrend[[cashDict['cagr5'],cashDict['cagr3']]].min(axis=1)
 
-class StockGrowht:
-    from base import stockYoYcsv
-    def __init__(self):
-        self.stockYoY=baseDfTrans(self.stockYoYcsv)
