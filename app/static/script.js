4bbc80f90d3b94412a786787908aea92ef44a4e0
Jesse Tseng <pkh_uuuu1112@photons.com.tw>
2023-09-01 13:23:31 +0800

    about home descrip

--- app/static/script.js
+++ app/static/script.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     const footerYear = document.getElementById("currentYear");
     const currentYear = new Date().getFullYear();
-    
     footerYear.textContent = currentYear;
 });
+
+
